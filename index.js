73ffef2109300e75dec0a441227f987539ddc855
EngrArfin <arfin.cse.green.edu.bd.gmail.com>
2023-08-31 23:54:01 +0600

    update server

--- index.js
+++ index.js
@@ -28,11 +28,82 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
+  
+/*     const serviceCollection = client.db('toyMarket').collection('services');
+    const bookingCollection = client.db('toyMarket').collection('toying');
+    const categoriesCollection = client.db('CourseLBD').collection('categories'); */
+
+
+    /*  app.get('/services', async(req, res) =>{
+      const cursor = serviceCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+    
+    app.get('/services/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id: new Object(id)}
+
+      const options = {
+        projection: { title: 1, price: 1, service_id: 1, img: 1},
+      }; 
+
+      const result = await serviceCollection.findOne(query, options);
+      res.send(result);
+
+    })
+ */
+
+    const courseCollection = client.db('CourseLBD').collection('courses');
+    const teachersCollection = client.db('CourseLBD').collection('teachers');
+    const categoriesCollection = client.db('CourseLBD').collection('categories');
+    //courses
+    app.get('/courses', async(req, res) =>{
+      const result = await courseCollection.find().toArray();
+      res.send(result);
+    })
+    app.post('/courses', async(req, res) =>{
+      const courses = req.body;
+      console.log(courses);
+      const result = await courseCollection.insertOne(courses);
+      res.send(result)
+
+    })
+
+    /* teachers */
+    app.get('/teachers', async(req, res) =>{
+      const result = await teachersCollection.find().toArray();
+      res.send(result);
+    })
+    app.post('/teachers', async(req, res) =>{
+      const teachers = req.body;
+      console.log(teachers);
+      const result = await teachersCollection.insertOne(teachers);
+      res.send(result)
+
+    })
+
+    /* categories */
+    app.get('/categories', async(req, res) =>{
+      const result = await categoriesCollection.find().toArray();
+      res.send(result);
+    })
+    app.post('/categories', async(req, res) =>{
+      const categories = req.body;
+      console.log(categories);
+      const result = await categoriesCollection.insertOne(categories);
+      res.send(result)
+
+    })
+
+   
+
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    //await client.close();
   }
 }
 run().catch(console.dir);
